a70915497c63cc83db6d0b96bbdb60b06dd71372
Gustavo Marques <gustavomarques.gml@gmail.com>
2021-07-26 15:12:02 -0300

    Creation of ProblemBody components

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home";
 import Problem from "./components/problem";
 import 'katex/dist/katex.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => (
   <Router>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/problem" component={Problem} />
--- src/components/problem/index.tsx
+++ src/components/problem/index.tsx
@@ -1,6 +1,7 @@
 import Latex from 'react-latex-next'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const LaTex = 'We give illustrations for the three processes $e^+e^-$, gluon-gluon and $\\gamma\\gamma \\to W t\\bar b$.'
+const content = 'Lorem ipsum \\(x_1\\) dolor \\[\\sin(\\alpha) + \\cos(\\theta)\\] sit amet, consectetur adipisicing elit...\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris...\nLorem ipsum dolor \\(x^2\\) sit amet, consectetur adipisicing elit...\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris...\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris...\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris...';
 
 const Problem = () => {
   return (
@@ -9,8 +10,92 @@ const Problem = () => {
         <h1>  <Latex>\(\gamma\)</Latex> Online Judge</h1>
         <p>Problemas e eventos de programação competitiva</p>
       </div>
-      <p><Latex>{LaTex}</Latex></p>
+      <div className="row justify-content-center">
+        <div className="col-2">
+          <Latex>{'\\(x_1\\)'}</Latex>
+        </div>
+        <ProblemContent
+          className="col-8"
+          title="A. Problem Title"
+          context={content}
+          constraints={["a>b"]}
+          sampleInputs={
+            [
+              {
+                title: "Sample input 1",
+                input: "1 2 3",
+                output: "a b c"
+              },
+              {
+                title: "Sample input 2",
+                input: "2 3 4",
+                output: "c d e"
+              }
+            ]
+          }
+        />
+      </div>
     </div>
   );
 };
 export default Problem;
+
+const ProblemContent = ({ className, title, context, constraints = [], sampleInputs = [] }: ProblemContentProps) => {
+  return (
+    <div className={className}>
+
+      <h1>{title}</h1>
+      <hr />
+      <p><Latex>{context}</Latex></p>
+      {(constraints.length !== 0) &&
+        <div>
+          <h2>Contrains</h2>
+          <div>
+            {constraints.map((constraint, i) => {
+              return (
+                <li key={i}><Latex>{constraint}</Latex></li>
+              )
+            })}
+          </div>
+        </div>
+      }
+      {
+        sampleInputs.length !== 0 &&
+        <div>
+          <hr />
+          {
+            sampleInputs.map((sample, i) => {
+              return (
+                <div key={i}>
+                  <h3>{sample.title}</h3>
+                  <div className="panel panel-default">
+                    <div className="panel-heading">Input</div>
+                    <div className="panel-body">{sample.input}</div>
+                  </div>
+                  <div className="panel panel-default">
+                    <div className="panel-heading">Output</div>
+                    <div className="panel-body">{sample.output}</div>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      }
+      <hr />
+    </div>
+  )
+}
+
+type ProblemContentProps = {
+  className?: string
+  title: string
+  context: string
+  constraints?: string[]
+  sampleInputs?: SampleInput[]
+}
+
+type SampleInput = {
+  title: string
+  input: string
+  output: string
+}
