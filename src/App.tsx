4e1d211360a46c437346bd2059505f48482d5e86
Gustavo Marques <gustavomarques.gml@gmail.com>
2021-07-26 10:25:52 -0300

    Addition of Latex module

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home";
 import Problem from "./components/problem";
+import 'katex/dist/katex.min.css'
 
 const App = () => (
   <Router>
--- src/components/problem/index.tsx
+++ src/components/problem/index.tsx
@@ -1,10 +1,15 @@
+import Latex from 'react-latex-next'
+
+const LaTex = 'We give illustrations for the three processes $e^+e^-$, gluon-gluon and $\\gamma\\gamma \\to W t\\bar b$.'
+
 const Problem = () => {
   return (
     <div className="problem-page-container">
       <div className="jumbotron text-center bg-dark text-white">
-        <h1>\(\gamma\) Online Judge</h1>
+        <h1>  <Latex>\(\gamma\)</Latex> Online Judge</h1>
         <p>Problemas e eventos de programação competitiva</p>
       </div>
+      <p><Latex>{LaTex}</Latex></p>
     </div>
   );
 };
