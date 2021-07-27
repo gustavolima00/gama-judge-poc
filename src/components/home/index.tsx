b55ad867667b160204c1b7dc57fd370a27320fa0
Gustavo Marques <gustavomarques.gml@gmail.com>
2021-07-27 09:45:21 -0300

    Correction of problem url

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h2> Home</h2>
-      <Link to="/problem">
-        <button>Problem</button>
+      <h2> Home </h2>
+      <Link to="/problem/123">
+        <button> Problem 123 </button>
       </Link>
     </div>
   );
